fe839b4a52ca72044faf201c486f2f99aa0943b1
trueParadise <para_paradisal@163.com>
2022-05-23 17:25:53 +0800

    [update] Em爬虫完成营业部和营业部详情数据爬虫

--- staff/crawler.py
+++ staff/crawler.py
@@ -41,7 +41,7 @@ class Em:
 
     @classmethod
     @lru_cache(maxsize=None, typed=False)
-    def active_opdep_em(cls, date: 'str | datetime.datetime') -> pd.DataFrame:
+    def active_opdep(cls, date: 'str | datetime.datetime') -> pd.DataFrame:
         '''Update data for active oprate department
         --------------------------------------------
 
@@ -68,10 +68,36 @@ class Em:
         data = pd.DataFrame(data)
         data = data.rename(columns=dict(zip(data.columns, data.columns.map(lambda x: x.lower()))))
         data.onlist_date = pd.to_datetime(data.onlist_date)
-        datas = pd.DataFrame()
-
+        return data
+    
+    @classmethod
+    @lru_cache(maxsize=None, typed=False)
+    def active_opdep_details(cls, date: 'str | datetime.datetime') -> pd.DataFrame:
+        date = time2str(date)
+        params = {
+            "sortColumns": "TOTAL_NETAMT,ONLIST_DATE,OPERATEDEPT_CODE",
+            "sortTypes": "-1,-1,1",
+            "pageSize": 100000,
+            "pageNumber": 1,
+            "reportName": "RPT_OPERATEDEPT_ACTIVE",
+            "columns": "ALL",
+            "source": "WEB",
+            "client": "WEB",
+            "filter": f"(ONLIST_DATE>='{date}')(ONLIST_DATE<='{date}')"
+        }
+        headers = {
+            "Referer": "https://data.eastmoney.com/"
+        }
+        res = Request(cls.__data_center, headers=headers, params=params).get().json
+        data = res['result']['data']
+        data = pd.DataFrame(data)
+        data = data.rename(columns=dict(zip(data.columns, data.columns.map(lambda x: x.lower()))))
+        data.onlist_date = pd.to_datetime(data.onlist_date)
+        data = data[['onlist_date', 'operatedept_code']]
+        datas = []
         for i in range(len(data)):
             opdep_code = data.iloc[i, :]['operatedept_code']
+            print(opdep_code)
             params = {
                 "sortColumns": "TRADE_DATE,SECURITY_CODE",
                 "sortTypes": "-1,1",
@@ -83,17 +109,20 @@ class Em:
                 "source": "WEB",
                 "client": "WEB"
             }
-
-            res = Request(cls.__data_center, params=params, headers=headers).get().json
-            data_details = res['result']['data']
-            data_details = pd.DataFrame(data_details)
-            data_details = data_details.rename(columns=dict(zip(data_details.columns, data_details.columns.map(lambda x: x.lower()))))
-            data_details['onlist_date'] = pd.to_datetime(data_details['onlist_date'])
-            # data_details = data_details.loc[data_details['onlist_date'] == date]
-            datas = pd.concat([datas, data_details], axis=0)
-        datas = datas.reset_index(drop=True)
+            headers = {
+                "Referer": "https://data.eastmoney.com/"
+            }
+            res = Request(cls.__data_center, headers=headers, params=params).get().json
+            data_sub = res['result']['data']
+            data_sub = pd.DataFrame(data_sub)
+            data_sub = data_sub.rename(columns=dict(zip(data_sub.columns, data_sub.columns.map(lambda x: x.lower()))))
+            data_sub.trade_date = pd.to_datetime(data_sub.trade_date)
+            data_sub = data_sub.loc[data_sub['trade_date'] == date]
+            datas.append(data_sub)
+            
+        datas = pd.concat(datas, axis=0)
         return datas
 
-
 if __name__ == "__main__":
-    data = Em.active_opdep_em('20210104')
+    data = Em.active_opdep_details('20210506')
+    data.to_csv('data.csv')
