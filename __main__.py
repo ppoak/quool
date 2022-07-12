f91d17de147060b360048bfcdde096f0acc278fe
DacianPeng <dacianpeng@gmail.com>
2022-07-12 10:37:59 +0800

    FIX: 修复了expire参数类型
    Co-authored-by: JustLikeATree <ppoak@users.noreply.github.com>

--- __main__.py
+++ __main__.py
@@ -9,7 +9,7 @@ subparser = parser.add_subparsers()
 setter = subparser.add_parser('set', help='Set cache via pandasquant Cli')
 setter.add_argument('-k', '--key', required=True, type=str, help='Key to the cache')
 setter.add_argument('-v', '--value', required=True, help='Value to the cache')
-setter.add_argument('-e', '--expire', default=None, help='Valid time, default to forever')
+setter.add_argument('-e', '--expire', default=None, type=int, help='Valid time, default to forever')
 setter.set_defaults(func=set)
 
 deleter = subparser.add_parser('delete', help='Delete cache via pandasquant Cli')
