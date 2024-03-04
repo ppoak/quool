551d9b6f3fc3c4fad31fa8d78810d4309304072c
ppoak <oakery@qq.com>
2024-03-04 20:32:37 +0800

    add different ways to add proxy

--- quool/spider.py
+++ quool/spider.py
@@ -1,26 +1,31 @@
+import re
 import time
 import base64
 import random
 import hashlib
 import requests
+import numpy as np
 import pandas as pd
+from lxml import etree
 from pathlib import Path
 from retrying import retry
 from .table import ItemTable
 from bs4 import BeautifulSoup
+from .util import parse_commastr
+from joblib import Parallel, delayed
 
 
 class ProxyManager(ItemTable):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124S Safari/537.36"}
 
     def __init__(
         self, 
         uri: str | Path, 
-        size: int = 100,
         create: bool = False,
     ):
-        self._size = size
-        self._invalid = []
+        self._size = 1000000
         super().__init__(uri, create)
+        self.forget()
     
     @property
     def spliter(self):
@@ -31,25 +36,158 @@ class ProxyManager(ItemTable):
         return lambda x: x.index[0].strftime(f"%Y-%m-%d")
 
     @property
-    def invalid(self):
-        return self._invalid
+    def picked(self):
+        return self._picked
     
-    def add_invalid(self, index: int):
-        self._invalid.append(index)
-    
-    def add_proxy(self, proxy: dict | list[dict]):
-        max_index = self._read_fragment(self.fragments[-1]).index.max()
-        proxy = [proxy] if not isinstance(proxy, list) else proxy
-        proxy = pd.DataFrame([proxy], index=range(max_index + 1, max_index + len(proxy) + 1))
-        self.update(proxy)
+    def forget(self):
+        self._fraglen = pd.Series(
+            np.ones(len(self.fragments)) * self._size,
+            index=self.fragments
+        )
+        self._picked = pd.Series([
+            set() for _ in self.fragments
+        ], index=self.fragments)
     
     def pick(self, field: str | list = None):
-        max_index = self._read_fragment(self.fragments[-1]).index.max()
-        index = random.choice(list(set(range(max_index + 1)) - set(self.invalid)))
-        return self.read(field, start=[index]).to_dict('records')[0]
+        rand_frag = self._fraglen[self._fraglen.cumsum() / 
+            self._fraglen.sum() > random.random()].index
+        if rand_frag.size == 0:
+            raise ValueError("no more proxies available")
+        proxy = self._read_fragment(rand_frag)
+        if field is not None:
+            proxy = proxy[field]
+        index = random.choice(list(set(range(proxy.shape[0])) - self._picked.loc[rand_frag]))
+        self._picked.loc[rand_frag].add(index)
+        self._fraglen.loc[rand_frag] = proxy.shape[0] - len(self._picked.loc[rand_frag])
+        return proxy.to_dict('records')[index]
 
+    def check(self, proxy: dict, timeout: int = 2):
+        check_url = "http://httpbin.org/ip"
+        try:
+            pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+            ip = re.findall(pattern, proxy['http'])[0]
+            ips = re.findall(pattern, proxy['https'])[0]
+            resp = requests.get(check_url, headers=self.headers, proxies=proxy, timeout=timeout)
+            resp = resp.json()
+            if resp.get("origin") in [ip, ips]:
+                return True
+            return False
+        except:
+            return False
 
-@retry()
+    def add_kxdaili(self, pages: int = 1):
+        url_base = "http://www.kxdaili.com/dailiip/2/{i}.html"
+
+        resps = []
+        for i in range(1, pages + 1):
+            resps.append(requests.get(url_base.format(i=i), headers=self.headers))
+        
+        results = []
+        for resp in resps:
+            tree = etree.HTML(resp.text)
+            for tr in tree.xpath("//table[@class='active']//tr")[1:]:
+                ip = "".join(tr.xpath('./td[1]/text()')).strip()
+                port = "".join(tr.xpath('./td[2]/text()')).strip()
+                proxy = {
+                    "http": "http://" + "%s:%s" % (ip, port),
+                    "https": "https://" + "%s:%s" % (ip, port)
+                }
+                if self.check(proxy):
+                    results.append(pd.Series(proxy, name=pd.to_datetime('now')))
+        
+        results = pd.concat(results, axis=1).T
+        if self.fragments:
+            self.update(results)
+        else:
+            self.add(results)
+    
+    def add_kuaidaili(self, pages: int = 1):
+        inha_base = 'https://www.kuaidaili.com/free/inha/{i}/'
+        intr_base = 'https://www.kuaidaili.com/free/intr/{i}/'
+
+        urls = []
+        for i in range(1, pages + 1):
+            for pattern in [inha_base, intr_base]:
+                urls.append(pattern.format(i=i))
+            
+        resps = []
+        for url in urls:
+            resps.append(requests.get(url, headers=self.headers))
+            time.sleep(1)
+        
+        results = []
+        for resp in resps:
+            tree = etree.HTML(resp.text)
+            proxy_list = tree.xpath('.//table//tr')
+            for tr in proxy_list[1:]:
+                proxy = {
+                    "http": "http://" + ':'.join(tr.xpath('./td/text()')[0:2]),
+                    "https": "http://" + ':'.join(tr.xpath('./td/text()')[0:2])
+                }
+                if self.check(proxy):
+                    results.append(pd.Series(proxy, name=pd.to_datetime('now')))
+        
+        results = pd.concat(results, axis=1).T
+        if self.fragments:
+            self.update(results)
+        else:
+            self.add(results)
+    
+    def add_ip3366(self, pages: int = 1):
+        base1 = 'http://www.ip3366.net/free/?stype=1&page={i}' 
+        base2 = 'http://www.ip3366.net/free/?stype=2&page={i}'
+
+        urls = []
+        for i in range(1, pages + 1):
+            for pattern in [base1, base2]:
+                urls.append(pattern.format(i=i))
+            
+        resps = []
+        for url in urls:
+            resps.append(requests.get(url, headers=self.headers))
+            time.sleep(1)
+        
+        results = []
+        for resp in resps:
+            text = resp.text
+            proxies = re.findall(r'<td>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})</td>[\s\S]*?<td>(\d+)</td>', text)
+            for proxy in proxies:
+                proxy = {"http": "http://" + ":".join(proxy), "https": "http://" + ":".join(proxy)}
+                if self.check(proxy):
+                    results.append(pd.Series(proxy, name=pd.to_datetime('now')))
+        
+        results = pd.concat(results, axis=1).T
+        if self.fragments:
+            self.update(results)
+        else:
+            self.add(results)
+
+    def add_89ip(self, pages: int = 1):
+        url_base = "https://www.89ip.cn/index_{i}.html"
+
+        resps = []
+        for i in range(1, pages + 1):
+            resps.append(requests.get(url_base.format(i=i), headers=self.headers))
+            time.sleep(1)
+        
+        results = []
+        for resp in resps:
+            proxies = re.findall(
+                r'<td.*?>[\s\S]*?(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})[\s\S]*?</td>[\s\S]*?<td.*?>[\s\S]*?(\d+)[\s\S]*?</td>',
+                resp.text
+            )
+            for proxy in proxies:
+                proxy = {"http": "http://" + ":".join(proxy), "https": "http://" + ":".join(proxy)}
+                if self.check(proxy):
+                    results.append(pd.Series(proxy, name=pd.to_datetime('now')))
+        
+        results = pd.concat(results, axis=1).T
+        if self.fragments:
+            self.update(results)
+        else:
+            self.add(results)
+
+@retry
 def get_spot_data(proxy_manager: ProxyManager = None) -> pd.DataFrame:
     url = "http://82.push2.eastmoney.com/api/qt/clist/get"
     params = {
