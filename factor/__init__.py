e10659ef3adfda274041362d106d223019e2e5bb
ppoak <oakery@qq.com>
2024-03-06 22:53:56 +0800

    add default start and stop, make factor a package

---
+++ factor/__init__.py
@@ -0,0 +1,4 @@
+from .minfreq import MinFreqFactor
+
+
+mf = MinFreqFactor("./data/minfreq", code_level="order_book_id", date_level="date")
--- factor/minfreq.py
+++ factor/minfreq.py
@@ -7,7 +7,7 @@ from joblib import Parallel, delayed
 
 class MinFreqFactor(quool.Factor):
 
-    def get_tail_volume_percent(self, start: str, stop: str) -> pd.DataFrame:
+    def get_tail_volume_percent(self, start: str = None, stop: str = None) -> pd.DataFrame:
         def _get(date: pd.Timestamp):
             data = config.fqtm.read("volume", start=date, stop=date + pd.Timedelta(days=1))
             tail_vol = data.between_time("14:31", "14:57").sum()
@@ -16,6 +16,8 @@ class MinFreqFactor(quool.Factor):
             res.name = date
             return res
             
+        start = start or pd.to_datetime('now').strftime(r"%Y-%m-%d")
+        stop = stop or pd.to_datetime('now').strftime(r"%Y-%m-%d")
         trading_days = config.fqtd.get_trading_days(start, stop)
         return pd.concat(Parallel(n_jobs=-1, backend='loky')(
             delayed(_get)(date) for date in tqdm(list(trading_days))
