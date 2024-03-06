d40d6dcc101aa1a144b8e18d6d20c3d459c4139a
ppoak <oakery@qq.com>
2024-03-06 13:47:30 +0800

    add config and MinFreqFactor

--- factor/minfreq.py
+++ factor/minfreq.py
@@ -1,24 +1,22 @@
 import quool
+import config
 import pandas as pd
-from pathlib import Path
+from tqdm import tqdm
 from joblib import Parallel, delayed
 
 
-qtmf = quool.Factor("data/quotes-min")
-qtdf = quool.Factor("data/quotes-day")
-
 class MinFreqFactor(quool.Factor):
 
-    def get_factor(
-        self, name: str, 
-        start: str, stop: str,
-    ):
-        trading_days = qtdf.get_trading_days(start, stop)
-        return Parallel(n_jobs=-1, backend='loky')(
-            getattr(self, name)(date) for date in trading_days
-        )
-
-    def tail_volume_percent(
-        self, date: pd.Timestamp
-    ) -> pd.DataFrame:
-        pass
+    def get_tail_volume_percent(self, start: str, stop: str) -> pd.DataFrame:
+        def _get(date: pd.Timestamp):
+            data = config.fqtm.read("volume", start=date, stop=date + pd.Timedelta(days=1))
+            tail_vol = data.between_time("14:31", "14:57").sum()
+            day_vol = data.sum()
+            res = tail_vol / day_vol
+            res.name = date
+            return res
+            
+        trading_days = config.fqtd.get_trading_days(start, stop)
+        return pd.concat(Parallel(n_jobs=-1, backend='loky')(
+            delayed(_get)(date) for date in tqdm(list(trading_days))
+        ), axis=1).T.loc[start:stop]
