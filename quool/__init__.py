2195d76fdd19c4a0a2f677649e83ba4200ed66e5
ppoak <oakery@qq.com>
2024-03-07 15:37:43 +0800

    remove factor from quool and add to factor package

--- quool/__init__.py
+++ quool/__init__.py
@@ -12,10 +12,6 @@ from .record import (
     ProxyRecorder,
 )
 
-from .factor import (
-    Factor
-)
-
 from .util import (
     Logger,
     parse_commastr,
@@ -29,4 +25,4 @@ WARNING = 30
 CRITICAL = 40
 
 
-__version__ = "5.0.3"
+__version__ = "5.0.4"
