2ec053b4c61556a8bbb28cf751f6000a4faa1f6a
ppoak <oakery@qq.com>
2024-03-07 15:25:05 +0800

    add some oprators in factor

---
+++ factor/ops.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+
+
+def zscore(df: pd.DataFrame):
+    return df.sub(df.mean(axis=1), axis=0
+        ).div(df.std(axis=1), axis=0)
+
+def minmax(df: pd.DataFrame):
+    return df.sub(df.min(axis=1), axis=0).div(
+        df.max(axis=1) - df.min(axis=1), axis=0)
+
+def madoutlier(
+    df: pd.DataFrame, 
+    dev: int, 
+    drop: bool = False
+):
+    median = df.median(axis=1)
+    ad = df.sub(median, axis=0)
+    mad = ad.abs().median(axis=1)
+    thresh_down = median - dev * mad
+    thresh_up = median + dev * mad
+    if not drop:
+        return df.clip(thresh_down, thresh_up, axis=0).where(~df.isna())
+    return df.where(
+        df.le(thresh_up, axis=0) & df.ge(thresh_down, axis=0),
+        other=np.nan, axis=0).where(~df.isna())
+
+def stdoutlier(
+    df: pd.DataFrame, 
+    dev: int, 
+    drop: bool = False
+):
+    mean = df.mean(axis=1)
+    std = df.std(axis=1)
+    thresh_down = mean - dev * std
+    thresh_up = mean + dev * std
+    if not drop:
+        return df.clip(thresh_down, thresh_up, axis=0).where(~df.isna())
+    return df.where(
+        df.le(thresh_up, axis=0) & df.ge(thresh_down, axis=0),
+        other=np.nan, axis=0).where(~df.isna())
+
+def iqroutlier(
+    df: pd.DataFrame, 
+    dev: int, 
+    drop: bool = False
+):
+    thresh_up = df.quantile(1 - dev / 2, axis=1)
+    thresh_down = df.quantile(dev / 2, axis=1)
+    if not drop:
+        return df.clip(thresh_down, thresh_up, axis=0).where(~df.isna())
+    return df.where(
+        df.le(thresh_up, axis=0) & df.ge(thresh_down, axis=0),
+        other=np.nan, axis=0).where(~df.isna())
