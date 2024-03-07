3f59fdac881fbdf76d75c889dc20b7383419d255
ppoak <oakery@qq.com>
2024-03-07 14:28:20 +0800

    distribute all config into each packages

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,14 @@
+import quool
 from .spider import (
     wechat_login,
     ewechat_notify,
     get_spot_return,
     get_spot_price,
 )
+
+
+qtd = quool.PanelTable("./data/quotes-day", code_level="order_book_id", date_level="date")
+qtm = quool.PanelTable("./data/quotes-min", code_level="order_book_id", date_level="datetime")
+fin = quool.PanelTable("./data/financial", code_level="order_book_id", date_level="date")
+idxwgt = quool.PanelTable("./data/index-weights", code_level="order_book_id", date_level="date")
+prx = quool.ProxyRecorder("./data/proxy")
