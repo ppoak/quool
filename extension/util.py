5802703f9bdb5ae3d92704d11e0a11dfd5a544d6
ppoak <oakery@qq.com>
2024-03-08 16:40:37 +0800

    add some plotting functions

--- extension/util.py
+++ extension/util.py
@@ -1,7 +1,9 @@
 import requests
+import squarify
 import pandas as pd
 import factor as f
 import database as d
+import matplotlib.pyplot as plt
 from retrying import retry
 
 
@@ -37,7 +39,7 @@ def get_spot_price() -> pd.DataFrame:
     return temp_df
 
 def get_spot_return(day: int = 1):
-    spot = d.get_spot_price()
+    spot = get_spot_price()
 
     if day <= 1:
         return spot
@@ -47,3 +49,40 @@ def get_spot_return(day: int = 1):
     price.index = price.index.str.slice(0, 6)
     spot["change_rate"] = (spot["latest_price"] / price - 1).dropna() * 100
     return spot
+
+def get_factor_value(fct: f.Factor, name: str, day: int = 0, processor: list = None):
+    last_date = f.fqtd.get_trading_days_rollback(rollback=day)
+    factor = fct.read(name, start=last_date, stop=last_date, processor=processor).squeeze()
+    return factor
+
+def treemap(size: pd.Series, nday: int = 0):
+    size.index = size.index.str.slice(0, 6)
+    size.name = "plotting_size"
+    data = pd.concat([size, get_spot_return(day=nday)], axis=1)
+    
+    bins = [-float('inf'), -20, -15, -10, -8, -7, -6, -5, -4,
+        -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 15, 20, float('inf')]
+    labels = [
+        "#00FF00", "#00EE00", "#00D000", "#00C000", "#00B000", "#00A000", 
+        "#009C00", "#007C00", "#005C00", "#003C00", "#002C00", 
+        "#001C00", "#1C0000",
+        "#2C0000", "#3C0000", "#5C0000", "#7C0000", "#9C0000", 
+        "#A00000", "#B00000", "#C00000", "#D00000", "#EE0000", "#FF0000"
+    ]
+    data["colors"] = pd.cut(data["change_rate"], bins=bins, labels=labels)
+    data = data.dropna(subset=["plotting_size", "change_rate"], axis=0, how='any')
+    data = data.sort_values(by="plotting_size", ascending=False)
+    fig, ax = plt.subplots(1, 1, figsize=(20, 20))
+    squarify.plot(sizes=data["plotting_size"], color=data["colors"], ax=ax, alpha=0.8)
+    ax.xaxis.set_visible(False); ax.yaxis.set_visible(False)
+    fig.tight_layout()
+    return fig
+
+def scatter(factor: pd.Series, nday: int = 1):
+    ret = get_spot_return(day=nday)
+    factor.index = factor.index.str.slice(0, 6)
+    data = pd.concat([factor, ret["change_rate"]], axis=1)
+    pd.plotting.scatter_matrix(data, alpha=0.5, figsize=(20, 20), hist_kwds={"bins": 100})
+    fig = plt.gcf()
+    fig.tight_layout()
+    return fig
