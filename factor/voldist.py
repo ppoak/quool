ef8580c3a24be5e5e42ae55ce79a61946149e2ae
ppoak <oakery@qq.com>
2024-03-17 18:32:18 +0800

    add foggy_amount_ratio factor

--- factor/voldist.py
+++ factor/voldist.py
@@ -25,3 +25,16 @@ class VolDistFactor(BaseFactor):
         res.name = date
         return res
 
+    def get_foggy_amount_ratio(self, date: pd.Timestamp) -> pd.Series:
+        price = fqtm.read("close", start=date, stop=date + pd.Timedelta(days=1))
+        vol = fqtm.read("volume", start=date, stop=date + pd.Timedelta(days=1))
+        amount = price * vol
+        ret = price.pct_change(fill_method=None)
+        std = ret.rolling(5).std()
+        blur = std.rolling(5).std()
+        foggy = blur[blur > blur.mean()]
+        foggyamt = amount.where(foggy.notna())
+        res = foggyamt.sum() / amount.sum()
+        res.name = date
+        return res
+
