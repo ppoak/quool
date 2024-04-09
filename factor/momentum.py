b228f07b86d3b813d01026868217fb0b444ab1a2
wiikai <whua5016@gmail.com>
2024-04-09 22:58:47 +0800

    rstr

--- factor/momentum.py
+++ factor/momentum.py
@@ -6,23 +6,16 @@ from base import (
 
     
 class MomentumFactor(BaseFactor):
-    def get_strev(self, date: str):
-        rollback = fqtd.get_trading_days_rollback(date, 21)
-        price = fqtd.read("close", start=rollback, stop=date)
-        ret  = np.log(1 + price.pct_change()).ewm(halflife=4).mean()
-        res = ret.rolling(21).mean().dropna(how='all')
-        return res
-    
-    def get_rstr(self, date: str):
-        rollback = fqtd.get_trading_days_rollback(date, 504)
-        price = fqtd.read("close", start=rollback, stop=date)
-        ret  = np.log(1 + price.pct_change()).ewm(halflife=126).mean()
-        res = ret.rolling(504).mean().dropna(how='all')
-        return res
-    
-    def get_momentum(self, date: str):
-        lt = self.get_rstr(date)
-        st = self.get_strev(date)
+    def get_rstr(self, date: str): # with a lag of L=21 tradingdays
+        rollback = fqtd.get_trading_days_rollback(date, 504+21)
+        prices = fqtd.read("close", start=rollback, stop=date)
+        ret  = np.log(1 + prices.pct_change()).ewm(halflife=126).mean()
 
-        res = lt - st 
-        return res.stack().droplevel(level='date')
+        for end_date in ret.index[21:]:
+            start_date = ret.index[ret.index.get_loc(end_date) - 504-21]
+            res = ret.loc[start_date:end_date].iloc[:-21]
+    
+        return res.sum()
+    
+
+    
