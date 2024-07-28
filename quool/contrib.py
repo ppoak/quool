53176266d5b5c2f97f7364b83e249f2cd5f78cc0
wiikai <whua5016@gmail.com>
2024-07-28 23:44:56 +0800

    Add package

--- quool/contrib.py
+++ quool/contrib.py
@@ -6,6 +6,7 @@ import itertools
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from tqdm import tqdm
 from lxml import etree
 from pathlib import Path
