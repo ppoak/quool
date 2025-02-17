62dba3675dc14bebe9ab6d4e6dd67add5dd6fe45
ppoak <ppoak@foxmail.com>
2025-02-17 19:37:43 +0800

    Add command line interface for Quool Terminal

--- quool/client/terminal.py
+++ quool/client/terminal.py
@@ -1,7 +1,9 @@
+import re
+from pathlib import Path
 from openai import OpenAI
-from quool import setup_logger
 from rich.console import Console
 from rich.markdown import Markdown
+from rich.prompt import Prompt, Confirm
 
 
 class Terminal:
@@ -12,20 +14,19 @@ class Terminal:
         base_url: str = "http://localhost:11434/v1",
         api_key: str = "ollama",
         prompt: str = "",
-        log_file: str = ".quool_history",
-        log_level: str = "INFO"
+        file: str = None,
     ):
-        self.console = Console()
+        self.console = Console(record=True)
         self.setup_client(base_url, api_key)
         self.setup_model(model)
         self.setup_prompt(prompt)
+        self.setup_file(file)
         self._lino = 1
-        self.logger = setup_logger("App", stream=False, level=log_level, file=log_file, clear=True)
         self.console.clear()
         self.console.print(Markdown(
             "# Quool\n\n"
             "Welcom to Quool Terminal\n\n"
-            "Try to talk ask anything you want to know\n\n"
+            "Try to ask anything you want to know\n\n"
         ))
         
     def setup_client(self, base_url: str, api_key: str):
@@ -40,12 +41,15 @@ class Terminal:
     def setup_prompt(self, prompt: str):
         self.prompt = prompt
         self._messages = [{"role": "system", "content": prompt}]
+    
+    def setup_file(self, file: str):
+        self.file = file
 
     def process_input(self, user_input: str):
         if user_input.startswith("%"):
-            self.process_command(user_input)
+            self.console.print(self.process_command(user_input))
         else:
-            self.process_chat(user_input)
+            self.console.print(self.process_chat(user_input))
     
     def process_command(self, user_input: str):
         commands = user_input[1:].split(" ")
@@ -55,12 +59,8 @@ class Terminal:
         else:
             self.console.print(f"Unknown command: {commands[0]}")
     
-    def command_show(self, name: str):
-        self.console.print(Markdown(f"**{name}**\n\n{getattr(self, name)}"))
-
     def process_chat(self, user_input: str):
         self._messages.append({"role": "user", "content": user_input})
-        self.logger.info(f"Q[{self._lino}]: {user_input}")
         response = self.client.chat.completions.create(
             messages=self._messages,
             model=self.model,
@@ -72,21 +72,31 @@ class Terminal:
             answer += content
             self.console.print(content, end="")
         self.console.print()
+        self.console.rule()
         self._messages.append({"role": "assistant", "content": answer})
-        self.logger.info(f"A[{self._lino}]: {answer}")
+        clear_answer = re.sub(r"<think>\s+", ">", answer)
+        clear_answer = re.sub(r"\s+</think>", "\n\n", clear_answer)
+        return Markdown(clear_answer)
 
     def run(self):
         while True:
             try:
-                user_input = self.console.input(f"[green]Q[{self._lino}]: [/green]")
+                user_input = Prompt.ask(f"\n[green]Q[{self._lino}][/green]", console=self.console)
                 if user_input:
                     self.process_input(user_input)
             except EOFError:
-                self.console.print("Exiting...")
+                self.console.print("[red]Exiting...[/red]")
+                self.command_save()
                 break
             except KeyboardInterrupt:
-                self.console.print("Interrupting...")
+                self.console.print("[yellow]Interrupting...[/yellow]")
             except Exception as e:
                 self.console.print_exception(show_locals=True)
             else:
                 self._lino += 1
+
+    def command_save(self):
+        if Confirm.ask("Do you want to export the conversation to a HTML file?", console=self.console, default='Y'):
+            if self.file is None:
+                self.file = Prompt.ask("Enter the file name", console=self.console)
+            Path(self.file).write_text(self.console.export_html(), encoding="utf-8")
